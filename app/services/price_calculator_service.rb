05ecdf07a4c32c427b57a67e984671277f66d24f
Marco <mgattelligit@gmail.com>
2025-03-15 10:09:56 +0100

    yard on services

--- app/services/price_calculator_service.rb
+++ app/services/price_calculator_service.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class PriceCalculatorService
+  # @param options [Array<Integer>] An array of part option IDs.
   def initialize(options)
     @price = 0.0
     @options = ::PartOption.find(options)
   end
 
+  # Computes the total price based on selected options and applicable price rules.
+  #
+  # @return [Float] The final computed price.
   def compute_price
     add_default_part_price
     add_composed_price
@@ -15,10 +19,16 @@ class PriceCalculatorService
 
   private
 
+  # Adds the base price of all selected part options to the total price.
+  #
+  # @return [void]
   def add_default_part_price
     @price += @options.sum(&:price)
   end
 
+  # Applies price rules for dependent part options and adjusts the total price.
+  #
+  # @return [void]
   def add_composed_price
     price_rules = ::PriceRule.for_base_option(@options)
     options_ids = @options.map(&:id)
--- app/services/validate_new_product_service.rb
+++ app/services/validate_new_product_service.rb
@@ -3,6 +3,7 @@
 class ValidateNewProductService
   include ActiveModel::Validations
 
+  # @param options [Array<Integer>] An array of selected option IDs.
   def initialize(options)
     @options = options
   end
@@ -10,15 +11,21 @@ class ValidateNewProductService
   # Validations
   validate :validate_restrictions
 
+  # Checks if any selected options are restricted from being chosen together.
+  #
+  # @return [void]
+  #
+  # @raise [ActiveModel::ValidationError] if any restrictions are violated.
   def validate_restrictions
     restriction_for_options = ::Restriction.for_dependent_option(@options)
 
     restriction_for_options.each do |restriction|
       if @options.include?(restriction.blocked_option_id.to_s)
-        errors.add(:base, <<~TEXT
+        error_text = <<~TEXT
           #{restriction.dependent_option.name} cannot be selected with #{restriction.blocked_option.name}
         TEXT
-        )
+
+        errors.add(:base, error_text)
       end
     end
   end
